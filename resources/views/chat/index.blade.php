2f83ab08bfa06bca1af2ba8a40c417245460df33
LittleElijah <cuiyeqing960904@gmail.com>
2018-05-22 00:00:20 +0300

    chat set up

---
+++ resources/views/chat/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8">
+            Chat
+        </div>
+
+        <div class="col-md-4">
+          users
+        </div>
+    </div>
+</div>
+@endsection
